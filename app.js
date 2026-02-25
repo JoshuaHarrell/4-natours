68c52c328510a3da1f8fdd5c699635b5a8469e49
harrelljoshy2 <harrelljoshy0427@gmail.com>
2026-02-25 09:18:47 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ const tours = JSON.parse(
   fs.readFileSync(`$__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {});
+app.get('/api/v1/tours', (req, res) => {
+  res.json(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
+});
 
 const port = 3000;
 app.listen(port, () => {
