f5ad3af8d0facbac0091650982d42d5c115a6659
harrelljoshy2 <harrelljoshy0427@gmail.com>
2026-02-25 11:37:46 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -74,13 +74,17 @@ app.post('/api/v1/tours', (req, res) => {
   });
 });
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+app.delete('/api/v1/tours/:id', (req, res) => {
   if (req.params.id* 1 > tours.length) 
   return res.status(404).json({
+    status: 'fail',
+    message: 'Invalid ID'
+  });
+
+  res.status(204).json({
     status: 'success',
-    message: '<Updated tour here...>'
-    
-  })
+    data: '<Updated tour here...>'
+  });
 });
 
 const port = 3000;
