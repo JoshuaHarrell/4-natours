488bcb7d48b849806e01b7a90f15a814dabe9651
harrelljoshy2 <harrelljoshy0427@gmail.com>
2026-02-26 11:33:13 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -14,10 +14,43 @@ const getAllTours = (req, res) => {
     status: 'success',
     results: tours.length,
     data: {
-      tour: '<tour updated>'
+      tours
     }
   });
 };
+
+const getTour = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  const tour = tours.find(el => el.id === id);
+}
+
+// if (id = tours length) {
+if (!tour) {
+  return res.status(404).json({
+    status: 'fail',
+    data: {
+      tour
+    }
+  });
+};
+
+const createTour = (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+     res.status(201).json 
+    }
+  )
+}
 
 const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
