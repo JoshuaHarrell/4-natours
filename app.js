a3fb94ee9962bdffc7578872cfbc424ed5f6201a
harrelljoshy2 <harrelljoshy0427@gmail.com>
2026-02-27 08:29:23 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -20,91 +20,6 @@ app.use((req, res, next) => {
 })
 
 // 2) ROUTE HANDLERS
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-
-  const tour = tours.find(el => el.id === id);
-}
-
-// if (id = tours length) {
-if (!tour) {
-  return res.status(404).json({
-    status: 'fail',
-    data: {
-      tour
-    }
-  });
-};
-
-const createTour = (req, res) => {
-  // console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-     res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-     });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  };
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>'
-    }
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-};
 
 const getAllUsers = (req, res) => {
   res.status(500).json({
@@ -142,14 +57,7 @@ const deleteUser = (req, res) => {
 };
 
 // 3) ROUTES
-const tourRouter = express.Router();
 const userRouter = express.Router();
-
-tourRouter
-.route('/api/v1/tours/:id')
-.get(getTour)
-.patch(updateTour)
-.delete(deleteTour);
 
 userRouter
 .route('/api/v1/users')
@@ -163,7 +71,7 @@ userRouter
 .delete(deleteUser);
 
 app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4) START SERVER
 const port = 3000;
