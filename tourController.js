2e8aefdfbc6b9582d2a37e861de04c99e1703fbb
harrelljoshy2 <harrelljoshy0427@gmail.com>
2026-03-03 13:44:56 -0500

    Initial Commit

--- tourController.js
+++ tourController.js
@@ -62,7 +62,9 @@ exports.createTour = async (req, res) => {
 
 exports.updateTour = (req, res) => {
   try {
-    
+    Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
     res.status(200).json({
     status: 'success',
     data: {
