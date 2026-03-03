548dab63912ae69dacae0c7ffb738b7de94f696b
harrelljoshy2 <harrelljoshy0427@gmail.com>
2026-03-03 08:57:31 -0500

    Initial Commit

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
 const app = require('./app');
+
+mongoose.connect();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
