b2dafe1aaabbc6e933f03dc6936188a037d62ee7
harrelljoshy2 <harrelljoshy0427@gmail.com>
2026-03-04 08:41:16 -0500

    Initial Commit

--- tourController.js
+++ tourController.js
@@ -74,21 +74,9 @@ exports.updateTour = async (req, res) => {
   });
 
   } catch (err) {
-
-  }
-  
-};
-
-exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID'
     });
   }
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
 };
