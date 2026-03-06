999917a5079cb7cd6dc2fb9e3b44f4a554fa97fb
harrelljoshy2 <harrelljoshy0427@gmail.com>
2026-03-06 12:45:04 -0500

    Initial Commit

--- tourController.js
+++ tourController.js
@@ -151,4 +151,43 @@ exports.getTourStats = async (req, res) => {
      message: err
    });
   }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.parms.year * 1; // 2021
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1},
+          tours: { $push: '$name' }
+        }
+      }
+    ]);
+
+    res.status(200).json({
+    status: 'success',
+    data: {
+      plan
+    }
+  });
+  } catch(err) {
+    res.status(404).json({
+     status: 'fail',
+     message: err
+   });
+  }
 }
