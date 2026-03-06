5c5fcc5adae3b0aa1f1283912c54dc91b12ffd4b
harrelljoshy2 <harrelljoshy0427@gmail.com>
2026-03-06 09:17:30 -0500

    Initial Commit

---
+++ getAllTours.js
@@ -0,0 +1,30 @@
+const Tour = require('./../modules/tourModel');
+const APIFeatures = require('./../utilis/apifeatures');
+
+exports.getAllTours = async (req, res) => {
+  try {
+    // EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query);
+    console.log(req.query);
+    filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
--- tourController.js
+++ tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../modules/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) = {
   req, query, limit: '5';
@@ -7,28 +8,25 @@ exports.aliasTopTours = (req, res, next) = {
   next();
 };
 
-
-
 exports.getAllTours = async (req, res) => {
   try {
-  // EXECUTE QUERY
-  const features = new APIFeatures(Tour.find(), req.query)
-  .filter()
-  .sort()
-  .limitFields()
-  .paginate(0);
-  const tours = await features.query;
+    // EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      limitFields()
+      .paginate();
+    const tours = await features.query;
 
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
-
-  } catch(err) {
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      } 
+    });
+  } catch (err) {
     res.status(404).json({
       status: 'fail',
       message: err
